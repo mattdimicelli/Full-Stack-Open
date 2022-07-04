3a3cc0323aed3bf0c83aaeb85a2099b8fa09fa49
Matthew Di Micelli <mrd2689a@gmail.com>
2022-07-04 17:14:38 -0400

    complete exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,21 +3,15 @@ const Header = ({course}) => <h1>{course}</h1>
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
   return(
     <>
-    <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
   </>
   );
 };
 
 const Footer = ({exercises1, exercises2, exercises3}) => <p>Number of exercises = {exercises1 + exercises2 + exercises3}</p>
-
+const Part = ({part, exercises}) => <p>{part} {exercises}</p>
 
 const App = () => {
   const course = 'Half Stack application development';
