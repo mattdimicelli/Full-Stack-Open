5dcfcfef9c5279c506dadd1447274a1751ffec61
Matthew Di Micelli <matthewdimicelli@gmail.com>
2022-07-08 19:41:09 -0400

    finish exercise 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,30 +2,8 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = good + bad + neutral
-
-  const handleClickGood = () => { setGood(good + 1) };
-
-  const handleClickNeutral = () => { setNeutral(neutral + 1) };
-
-  const handleClickBad = () => { setBad(bad + 1) };
-
+const Statistics = ({good, bad, neutral, all}) => {
   return (
-    <div>
-      <h1>give feedback</h1>
-
-      <div>
-        <button onClick={handleClickGood}>good</button>
-        <button onClick={handleClickNeutral}>neutral</button>
-        <button onClick={handleClickBad}>bad</button>
-      </div>
-
       <table>
         <caption>statistics</caption>
         <tbody>
@@ -55,6 +33,32 @@ const App = () => {
           </tr>
         </tbody>
       </table>
+  )
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const all = good + bad + neutral
+
+  const handleClickGood = () => { setGood(good + 1) };
+
+  const handleClickNeutral = () => { setNeutral(neutral + 1) };
+
+  const handleClickBad = () => { setBad(bad + 1) };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <div>
+        <button onClick={handleClickGood}>good</button>
+        <button onClick={handleClickNeutral}>neutral</button>
+        <button onClick={handleClickBad}>bad</button>
+      </div>
+      <Statistics bad={bad} good={good} neutral={neutral} all={all} />
+
     </div>
   )
 }
