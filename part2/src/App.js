d35b2f1c9937c925768bc8b2a76d556d333283e0
MDiMicelli <mdimicelli@northwell.edu>
2022-10-26 10:49:16 -0400

    finish exercise 2.5

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,32 +1,5 @@
-const Header = ({ title }) => <h2>{title}</h2>;
 
-const Content = ({ parts }) => {
-    const rows = parts.map(({name, exercises, id}) => <tr key={id}><th>{name}</th><td>{exercises}</td></tr>);
-    const totalExercises = parts.reduce((acc, curr) => { 
-      return acc + curr.exercises 
-    }, 0);
-    // let totalExercises = 0;
-    // parts.forEach(part => totalExercises += part.exercises);
-    return (
-      <table>
-        <tbody>
-          {rows}
-          <tr><th>Total: </th><td>{totalExercises}</td></tr>
-        </tbody>
-      </table>
-    )
-};
-
-const Course = ({ course }) => {
-  const { name: title, id, parts } = course;
-
-  return (
-    <>
-      <Header title={title} />
-      <Content parts={parts} />
-    </>
-  );
-}
+import Course from './components/Course';
 
 
 const App = () =>  {
